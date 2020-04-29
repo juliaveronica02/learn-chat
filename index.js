7813e2377608db4fe02d1b67b97e51a7c204a07d
Julia Veronica <julia.veronicagdu@gmail.com>
2020-04-29 11:35:16 +0700

    simple chat done

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const app = require("express")();
 const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Chat App</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("a user is disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log(socket);
+    console.log("message : " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
 http.listen(4000, () => {
   console.log("listening on port 4000");
 });
